df83db5fc344388b29cd4e510cc0d230d44178de
Hannes Duske <152866008+hannesduske@users.noreply.github.com>
2024-12-02 17:26:11 +0100

    the FileManager now accepts paths relative to the home directory

--- src/utils/FileManager.cpp
+++ src/utils/FileManager.cpp
@@ -3,10 +3,68 @@
 #include <fstream>
 #include <iostream>
 #include <filesystem>
+#include <cstdlib>
+
+#include "Logger.hpp"
 
 namespace eduart{
 
 namespace filemanager{
+
+//==================================================
+// PathHandler
+//==================================================
+
+bool PathHandler::checkDirectory(std::string path){
+
+    return checkDirectory(std::filesystem::path(path));
+}
+
+bool PathHandler::checkDirectory(std::filesystem::path path){
+
+    if (!std::filesystem::exists(path)){
+        logger::Logger::getInstance()->log(logger::LogVerbosity::Debug, std::string("Directory " + path.u8string() + " doesn't exist, creating the directory now."));
+        if(!std::filesystem::create_directories(path)){
+            if (!std::filesystem::exists(path)){
+                logger::Logger::getInstance()->log(logger::LogVerbosity::Warning, std::string("Creating directory " + path.u8string() + " failed!"));
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+
+std::filesystem::path PathHandler::resolvePath(const std::string path){
+    std::filesystem::path resolved_path;
+
+    // absolute path -> use it directly
+    if (std::filesystem::path(path).is_absolute()) {
+        resolved_path = std::filesystem::path(path);
+    } else {
+
+        // relative path -> resolve it against home directory
+        const char* home = std::getenv("HOME");
+        if (home != nullptr) {
+            // unix
+            std::filesystem::path home_dir(home);
+            resolved_path = home_dir / path;
+        } else {
+            // windows
+            const char* user_profile = std::getenv("USERPROFILE");
+            if (user_profile != nullptr) {
+                std::filesystem::path user_profile_dir(user_profile);
+                resolved_path = user_profile_dir / path;
+            } else {
+                // use relative path
+                resolved_path = std::filesystem::current_path() / path;
+            }
+        }
+    }
+
+    return resolved_path;
+}
 
 //==================================================
 // VectorHandler
@@ -19,10 +77,10 @@ bool VectorHandler<T>::saveVectorToFile(const std::string filename, const std::v
 
 template<typename T>
 bool VectorHandler<T>::saveVectorToFile(const std::string filepath, const std::string filename, const std::vector<T> &vec){
-    
     if(vec->empty()) return false;
+    if(!PathHandler::checkDirectory(filepath)) return false;
     
-    std::string full_path = std::filesystem::path(filepath) / filename;
+    std::filesystem::path full_path = PathHandler::resolvePath(filepath)/ filename;
     std::ofstream file(full_path, std::ios::trunc);
     
     if(!file) return false;
@@ -45,7 +103,7 @@ template<typename T>
 bool VectorHandler<T>::readVectorFromFile(const std::string filepath, const std::string filename, std::vector<T> &vec){
     
     vec.erase();
-    std::string full_path = std::filesystem::path(filepath) / filename;
+    std::filesystem::path full_path = PathHandler::resolvePath(filepath) / filename;
     std::ifstream file(full_path);
     
     if(!file) return false;
@@ -74,8 +132,9 @@ bool ArrayHandler<T, l>::saveArrayToFile(const std::string filename, const std::
 template<typename T, std::size_t l>
 bool ArrayHandler<T, l>::saveArrayToFile(const std::string filepath, const std::string filename, const std::array<T, l> &arr){
     if(arr.empty()) return false;
+    if(!PathHandler::checkDirectory(filepath)) return false;
     
-    std::string full_path = std::filesystem::path(filepath) / filename;
+    std::filesystem::path full_path = PathHandler::resolvePath(filepath)/ filename;
     std::ofstream file(full_path, std::ios::trunc);
     
     if(!file) return false;
@@ -99,7 +158,7 @@ bool ArrayHandler<T, l>::readArrayFromFile(const std::string filepath, const std
         
     arr.fill(0);
 
-    std::string full_path = std::filesystem::path(filepath) / filename;
+    std::filesystem::path full_path = PathHandler::resolvePath(filepath) / filename;
     std::ifstream file(full_path);
     
     if(!file) return false;
@@ -129,8 +188,9 @@ bool StructHandler<T>::saveStructToFile(const std::string filename, const T &str
 
 template<typename T>
 bool StructHandler<T>::saveStructToFile(const std::string filepath, const std::string filename, const T &str){
-
-    std::string full_path = std::filesystem::path(filepath) / filename;
+    if(!PathHandler::checkDirectory(filepath)) return false;
+    
+    std::filesystem::path full_path = PathHandler::resolvePath(filepath)/ filename;
     std::ofstream file(full_path, std::ios::trunc);
     
     if(!file) return false;
@@ -149,7 +209,7 @@ bool StructHandler<T>::readStructFromFile(const std::string filename, T &str){
 template<typename T>
 bool StructHandler<T>::readStructFromFile(const std::string filepath, const std::string filename, T &str){
 
-    std::string full_path = std::filesystem::path(filepath) / filename;
+    std::filesystem::path full_path = std::filesystem::path(filepath) / filename;
     std::ifstream file(full_path);
     
     if(!file) return false;
