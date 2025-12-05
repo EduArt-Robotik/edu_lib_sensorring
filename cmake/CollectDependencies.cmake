616e65ddd808b90817b1399af05737687111293b
Hannes Duske <152866008+hannesduske@users.noreply.github.com>
2025-12-05 15:28:07 +0100

    build: set USBtingo build options

--- cmake/CollectDependencies.cmake
+++ cmake/CollectDependencies.cmake
@@ -1,23 +1,35 @@
 if(SENSORRING_USE_USBTINGO)
   find_package(usbtingo QUIET)
 
-  if(NOT usbtingo_FOUND)
+  if(usbtingo_FOUND)
+    set(SENSORRING_USBTINGO_INSTALLED ON)
+  else()
     message(STATUS "Did not find libusbtingo. Fetching it from GitHub...")
-
-    set(USBTINGO_BUILD_SHARED_LIBS OFF)
-    set(USBTINGO_BUILD_EXAMPLES OFF)
-    set(USBTINGO_BUILD_UTILS OFF)
-    set(USBTINGO_BUILD_TESTS OFF)
+    set(SENSORRING_USBTINGO_INSTALLED OFF)
 
     include(FetchContent)
     FetchContent_Declare(
       usbtingo
       URL      https://github.com/hannesduske/libusbtingo/archive/refs/heads/master.zip
       DOWNLOAD_EXTRACT_TIMESTAMP OFF
+      EXCLUDE_FROM_ALL
     )
-    
+
+    set(USBTINGO_BUILD_SHARED_LIBS OFF)
+    set(USBTINGO_BUILD_EXAMPLES OFF)
+    set(USBTINGO_BUILD_UTILS OFF)
+    set(USBTINGO_BUILD_TESTS OFF)
+    set(USBTINGO_INSTALL OFF)
+
     FetchContent_MakeAvailable(usbtingo)
-    add_library(usbtingo::usbtingo ALIAS usbtingo)
+
+    # Do NOT install usbtingo
+    set_property(TARGET usbtingo PROPERTY
+      EXCLUDE_FROM_ALL ON
+      EXCLUDE_FROM_DEFAULT_BUILD ON
+    )
+
+    add_library(usbtingo::usbtingo ALIAS usbtingo)   
 
   endif()
 endif()
