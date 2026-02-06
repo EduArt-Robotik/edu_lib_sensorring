bf717c1c2f8220fadaf9b7651d4f0dbc048af103
Hannes Duske <152866008+hannesduske@users.noreply.github.com>
2026-02-06 23:29:21 +0100

    docs: add doxygen comments to IDevice helpers

--- include/sensorring/device/CapabilityException.hpp
+++ include/sensorring/device/CapabilityException.hpp
@@ -1,3 +1,12 @@
+// Copyright (c) 2025 EduArt Robotik GmbH
+
+/**
+ * @file   CapabilityException.hpp
+ * @author EduArt Robotik GmbH
+ * @brief  Exception type for unsupported capability requests on IDevice.
+ * @date   2025-02-06
+ */
+
 #pragma once
 
 #include <stdexcept>
@@ -15,7 +24,13 @@ namespace device {
  * @brief Exception thrown when a requested capability type is not supported.
  */
 struct SENSORRING_EXPORT CapabilityNotSupported : std::runtime_error {
+  /// std::type_index of the unsupported capability.
   std::type_index capability_type;
+
+  /**
+   * @brief Construct exception for the given capability type.
+   * @param[in] cap std::type_index of the unsupported capability.
+   */
   CapabilityNotSupported(std::type_index cap)
       : std::runtime_error(std::string("Capability not supported: ") + cap.name())
       , capability_type(cap) {}
--- include/sensorring/device/DeviceID.hpp
+++ include/sensorring/device/DeviceID.hpp
@@ -1,3 +1,12 @@
+// Copyright (c) 2025 EduArt Robotik GmbH
+
+/**
+ * @file   DeviceID.hpp
+ * @author EduArt Robotik GmbH
+ * @brief  Device type enumeration and identifier (type, name, index) for device registration.
+ * @date   2025-02-06
+ */
+
 #pragma once
 
 #include <string>
@@ -8,6 +17,7 @@ namespace eduart {
 
 namespace device {
 
+/** @brief Supported device hardware types. */
 enum class DeviceType {
   VL53L8CX,
   HTPA32,
@@ -15,12 +25,25 @@ enum class DeviceType {
   UNDEFINED
 };
 
+/**
+ * @class DeviceID
+ * @brief Identifier for a device: type, human-readable name, and index.
+ */
 struct SENSORRING_EXPORT DeviceID {
 public:
+  /// Hardware device type.
   DeviceType type = DeviceType::UNDEFINED;
+
+  /// Human-readable device name.
   std::string name = "";
+
+  /// Instance index when multiple devices of same type exist.
   unsigned int index = 0;
 
+  /**
+   * @brief True if type is not UNDEFINED and name is non-empty.
+   * @return true if this ID represents a valid device.
+   */
   bool isValid() const;
 };
 
