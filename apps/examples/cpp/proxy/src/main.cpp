4b9688b7732c7557a39cf937e6010a3831bcaaf2
Hannes Duske <152866008+hannesduske@users.noreply.github.com>
2026-02-16 13:49:00 +0100

    feat: add a c++ example showing a simple proxy class implementation (corresponds to the old minimal example)

---
+++ apps/examples/cpp/proxy/src/main.cpp
@@ -0,0 +1,82 @@
+// Copyright (c) 2025 EduArt Robotik GmbH
+
+/**
+ * @file   main.cpp
+ * @author EduArt Robotik GmbH
+ * @brief  This example receives measurements and prints the current measurement rate to the command line.
+ * @date 2025-11-18
+ */
+
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <sensorring/manager/MeasurementManager.hpp>
+#include <thread>
+
+#include "MeasurementProxy.hpp"
+
+using namespace eduart;
+using namespace std::chrono_literals;
+
+static constexpr std::string_view INTERFACE_NAME   = "can0";
+static constexpr com::InterfaceType INTERFACE_TYPE = com::InterfaceType::SOCKETCAN;
+
+int main(int, char*[]) {
+  std::cout << "==============================" << std::endl;
+  std::cout << "Proxy class sensorring example" << std::endl;
+  std::cout << "==============================" << std::endl;
+  std::cout << std::endl;
+
+  // Create the parameter structure that is used to instantiate the sensorring
+  manager::ManagerParams params;
+  ring::RingParams ring;
+  {
+    device::VL53L8CX_Params tof;
+    tof.user_idx = 0;
+    tof.enable   = true;
+
+    device::SensorBoardParams board;
+    board.vl53l8cx_params = tof;
+
+    bus::BusParams bus;
+    bus.interface_name = INTERFACE_NAME;
+    bus.type           = INTERFACE_TYPE;
+    bus.board_param_vec.push_back(board);
+
+    ring.bus_param_vec.push_back(bus);
+  }
+
+  // Instantiate a Measurement proxy
+  auto proxy = std::make_unique<MeasurementProxy>();
+
+  try {
+    // Create SensorRing from ring params, then instantiate MeasurementManager
+    auto sensor_ring = ring::SensorRing::create(ring);
+    auto manager     = std::make_unique<manager::MeasurementManager>(params, std::move(sensor_ring));
+
+    // Register the proxy with the LogMeasurementManager to get the measurements
+    proxy->registerClient(manager.get());
+
+    // Start the measurements
+    manager->startMeasuring();
+
+    while (!proxy->gotFirstMeasurement() && manager->isMeasuring()) {
+    }
+
+    if (manager->isMeasuring()) {
+      std::cout << std::endl << "Printing measurement rate:" << std::endl;
+      while (manager->isMeasuring()) {
+        std::cout << "Current rate: " << std::fixed << std::setprecision(2) << std::setw(5) << proxy->getRate() << " Hz\r" << std::flush;
+        std::this_thread::sleep_for(1s);
+      }
+
+      // Stop the measurements
+      manager->stopMeasuring();
+    }
+
+  } catch (const std::exception& e) {
+    std::cout << "Caught: " << e.what() << std::endl;
+  }
+
+  return 0;
+}
