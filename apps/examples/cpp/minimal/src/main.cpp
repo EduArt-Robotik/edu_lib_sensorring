77a5115351dbcf99a5a9a9e785d67baa1a44d87e
Hannes Duske <152866008+hannesduske@users.noreply.github.com>
2026-02-16 15:48:20 +0100

    fix: use new DeviceTypes enum in minimal example

--- apps/examples/cpp/minimal/src/main.cpp
+++ apps/examples/cpp/minimal/src/main.cpp
@@ -96,7 +96,7 @@ int main(int, char*[]) {
     });
 
     // Subscribe to the ToF device group to get the measurements
-    auto tof_sub = manager->subscribeToDeviceGroup(manager::DeviceGroup::VL53L8CX, [&rate](const device::DeviceGroup&) {
+    auto tof_sub = manager->subscribeToDeviceGroup(device::DeviceType::VL53L8CX, [&rate](const device::DeviceGroup&) {
       rate->tick();
     });
 
