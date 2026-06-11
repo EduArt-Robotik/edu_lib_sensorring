b41b5adc38abb7caa2cbaa5b07664620d192392b
Hannes Duske <152866008+hannesduske@users.noreply.github.com>
2026-06-11 17:31:35 +0200

    fix(vl53l8cx): correct DepthSensor config

--- src/device/depth/vl53l8cx/VL53L8CX_Device.cpp
+++ src/device/depth/vl53l8cx/VL53L8CX_Device.cpp
@@ -18,7 +18,7 @@ namespace device {
 // clang-format off
 VL53L8CX_Device::VL53L8CX_Device(VL53L8CX_Params params, com::ComInterfaceID interface, unsigned int idx)
     : BaseDevice( DeviceID({ DeviceType::VL53L8CX, idx}), com::ComManager::getInstance()->getInterface(interface), com::ComEndpoint{ com::Direction::Output, static_cast<std::uint8_t>(idx + 1), devbyte::VL53L8CX }, params.enable)
-    , DepthSensor{ { true, true, false }, 45.0, 45.0, 8, 8 },
+    , DepthSensor{ { true, true, true }, 45.0, 45.0, 8, 8 },
     _impl(std::make_unique<VL53L8CX_DeviceImpl>(*this, params, com::ComManager::getInstance()->getInterface(interface), idx)) {
 }
 // clang-format on
