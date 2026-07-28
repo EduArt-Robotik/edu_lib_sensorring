ffc1358e16b9eec7493550168ccccc03a00e1b0f
Hannes Duske <152866008+hannesduske@users.noreply.github.com>
2026-07-28 16:14:46 +0200

    refactor(light): refine light command handling

--- src/device/light/Light.cpp
+++ src/device/light/Light.cpp
@@ -10,6 +10,12 @@ Light::Light(DeviceID id, com::ComInterface* interface, com::ComEndpoint target)
     : Device(id, interface, target) {
 }
 
+void Light::setAllLights(const std::vector<Light*>& devices, LightMode mode, std::uint8_t red, std::uint8_t green, std::uint8_t blue) {
+  for (auto* dev : devices) {
+    dev->setLight(mode, red, green, blue);
+  }
+}
+
 } // namespace device
 
 } // namespace sensorring
--- src/device/light/ws2812b/WS2812b_Device.cpp
+++ src/device/light/ws2812b/WS2812b_Device.cpp
@@ -15,42 +15,34 @@ namespace device {
 
 WS2812b_Device::WS2812b_Device(WS2812b_Params params, com::ComInterfaceID interface, unsigned int idx)
     : Light(DeviceID({ DeviceType::WS2812b, idx }), com::ComManager::getInstance()->getInterface(interface), com::ComEndpoint{ com::Direction::Output, static_cast<std::uint8_t>(idx + 1), devbyte::WS2812B })
-    , _params(std::move(params)) {
+    , _params(std::move(params))
+    , _last_setting({}) {
+}
+
+bool WS2812b_Device::configure() {
+  setLight(_last_setting.mode, _last_setting.red, _last_setting.green, _last_setting.blue);
+  return true;
 }
 
 void WS2812b_Device::setLight(LightMode mode, std::uint8_t r, std::uint8_t g, std::uint8_t b) {
+  _last_setting  = {mode, r, g, b};
+
   auto* iface     = _interface;
   auto board_addr = static_cast<std::uint8_t>(_hw_idx + 1);
-  setReplacableAction([mode, r, g, b, iface, board_addr]() {
+  execute("setLight", [mode, r, g, b, iface, board_addr]() {
     std::uint8_t mode_cmd       = static_cast<uint8_t>(mode);
     std::vector<uint8_t> tx_buf = { mode_cmd, r, g, b };
     iface->send(com::ComEndpoint{ com::Direction::Input, board_addr, devbyte::WS2812B }, SET_LED_MODE, tx_buf);
   });
 }
 
-bool WS2812b_Device::configure() {
-  // Broadcast OFF to all boards (turns off non-managed boards too)
-  setAllLights(LightMode::Off, 0, 0, 0);
-  return true;
-}
-
-bool WS2812b_Device::setAllLights(LightMode mode, std::uint8_t red, std::uint8_t green, std::uint8_t blue) {
-  std::uint8_t mode_cmd       = static_cast<uint8_t>(mode);
-  std::vector<uint8_t> tx_buf = { mode_cmd, red, green, blue };
-
-  for (auto& interface : com::ComManager::getInstance()->getInterfaces()) {
-    interface->send(com::ComEndpoint{ com::Direction::Broadcast, com::ComEndpoint::BROADCAST, devbyte::WS2812B }, SET_LED_MODE, tx_buf);
-  }
-  return true;
-}
-
-bool WS2812b_Device::syncLight() {
-  std::vector<uint8_t> tx_buf = {};
-
-  for (auto& interface : com::ComManager::getInstance()->getInterfaces()) {
-    interface->send(com::ComEndpoint{ com::Direction::Broadcast, com::ComEndpoint::BROADCAST, devbyte::WS2812B }, SYNCHRONIZE, tx_buf);
-  }
-  return true;
+void WS2812b_Device::syncLight() {
+  globalExecute([]() {
+    std::vector<uint8_t> tx_buf = {};
+    for (auto& interface : com::ComManager::getInstance()->getInterfaces()) {
+      interface->send(com::ComEndpoint{ com::Direction::Broadcast, com::ComEndpoint::BROADCAST, devbyte::WS2812B }, SYNCHRONIZE, tx_buf);
+    }
+  });
 }
 
 void WS2812b_Device::comCallback(const com::ComEndpoint, std::uint8_t, const std::vector<uint8_t>&) {
